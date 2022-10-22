5bc966f13f1104ad7d9c0dbe81b6ba7dfc673dc2
andre <andreas.bpdl@gmail.com>
2022-10-22 19:39:18 +0200

    hi there

--- list_parallel.py
+++ list_parallel.py
@@ -1,11 +1,10 @@
 while True:
-	y = 0
-	print("Parallelrechner (Widerstände mit + addieren):")
-	someList = input().split("+")
-	for i in someList:
-		x = float(i)
-		y += 1/x
-	print("Gesamtwiderstand:")
-	print(1/y)
-	pass
-	
+    y = 0
+    print("Parallelrechner (Widerstände mit + addieren):")
+    someList = input().split("+")
+    for i in someList:
+        x = float(i)
+        y += 1 / x
+    print("Gesamtwiderstand:")
+    print(1 / y)
+    pass
